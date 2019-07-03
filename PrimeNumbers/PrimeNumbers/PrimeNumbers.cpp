87b6919cd74a2e43dfea7d097471e6018c5e6519
Joshua Sew-Hee <joshua.sewhee@gmail.com>
2019-07-03 01:19:50 -0500

    Updaded PrimeNumbers code The function to determine all numbers who the prime factors, when added together, sum up to a prime number was implemented.

--- PrimeNumbers/PrimeNumbers/PrimeNumbers.cpp
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.cpp
@@ -1,34 +1,87 @@
 /*
-Name: Joshua Sew-Hee
-Date: 7/2/19
-Desc: Prime Numbers
+ * Name: Joshua Sew-Hee
+ * Date: 7/2/19
+ * Desc: Prime Numbers
+ * Prob: Design a program that finds all numbers from 1 to N whose prime factors,
+ *		 when added together, sum up to a prime number (for eg. 12 has prime 
+ *		 factors 2, 3, and 3, which sums to 7, which is prime.
+ */
 
-This program allows the user to enter a limit of desired prime numbers and displays them.
-*/
 #include <iostream>
+#include "PrimeNumbers.h"
 
 using namespace std;
 
+// This function determines if a number is divisible.
 bool isDivisible(int number, int divisor)
 {
 	return (number % divisor) == 0;
 }
 
-
+// This function determines if a number is prime.
 bool isPrime(int number)
 {
-	for (int i = 2; i < number; i++)
+	if (number < 2)
 	{
-		if (isDivisible(number, i))
-		{
-			return false;
-		}
+		cerr << "Error: Prime number cannot be less than 2.\n";
+		return false;
 	}
-	return true;
+	else
+	{
+		for (int i = 2; i < number; i++)
+		{
+			if (isDivisible(number, i))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
+// This function displays N prime numbers.
+void displayPrimeNumbers(int limit)
+{
+	int countCol = 0; // Keep track of columns to be printed
+	// Need to hardcode the first prime number for formatting issue for the remaining ones
+	if (limit > 1) {
+		cout << "2";
+	}
+	else {
+		cerr << "Prime numbers start from 2.\n";
+	}
+
+	// Limit is limit+1 one to display prime number if it is the limit, for eg. 7
+	for (int i = 2; i < limit + 1; i++)
+	{
+		if (isPrime(i))
+		{
+			if (countCol == COLUMNS)
+			{
+				cout << "\t\n" << i;
+				countCol = 0;
+
+			}
+			else
+			{
+				cout << "\t" << i;
+				countCol++;
+			}
+
+		}
+		/*	if (isPrime(i))
+			{
+				cout << "Prime number " << primeNumberCount << ": " << i << "\n";
+				primeNumberCount++;
+			}*/
+	}
+	cout << "\n";
+}
+
+// This function displays the prime factors of a number.
 void displayPrimeFactors(int n)
 {
+	cout << "Prime factors of " << n << " are: ";
 	// Store the number of 2s that divide n
 	// This gets rid of all multiples of 2
 	while ((n % 2) == 0)
@@ -54,8 +107,10 @@ void displayPrimeFactors(int n)
 	{
 		cout << n << " ";
 	}
+	cout << "\n";
 }
 
+// This function computes the sum of the prime factors of a number.
 int sumOfPrimeFactors(int n)
 {
 	// Initialize sum to zero
@@ -78,54 +133,79 @@ int sumOfPrimeFactors(int n)
 		{
 			sum = sum + i;
 			n = n / i;
-			
 		}
 	}
 
 	// This condition hanles the case when n is prime number > 2
 	if (n > 2)
 	{
-		cout << n << " ";
 		sum = sum + n;
 	}
 	return sum;
 }
 
+// This function displays the sum of prime factors of a number.
+void displaySumOfPrimeFactors(int limit)
+{
+	int sumPF = sumOfPrimeFactors(limit);
+	if (sumPF == 2) {
+		cout << "Sum of prime factors of " << limit << " is " << sumPF << "\n";
+	}
+	else
+	{
+		cout << "Sum of prime factors of " << limit << " are " << sumPF << "\n";
+	}
+}
+
+// This function displays all numbers from 1 to limit whose prime factors, when
+// added together, sum up to a prime number.
+void displayPrimeFactorsSumPrime(int limit)
+{
+	int countCol = 0;
+	// Need to hardcode the first prime number for formatting issue for the remaining ones
+	if (limit > 1) {
+		cout << "All number from 1 to " << limit << " whose prime factors, when " <<
+			"added together, sum up to a prime number.\n";
+		cout << "2";
+	}
+	else {
+		cerr << "Error: Prime numbers start from 2.\n";
+	}
+
+	// This for loop starts at 3 since 2 has already been taken care of above.
+	for (int i = 3; i < limit+1; i++)
+	{
+		if (isPrime(sumOfPrimeFactors(i)))
+		{
+			if (countCol == COLUMNS)
+			{
+				cout << "\t\n" << i;
+				countCol = 0;
+			}
+			else
+			{
+				cout << "\t" << i;
+				countCol++;
+			}
+			//cout << i << "\n";
+		}
+	}
+}
 
 int main()
 {
-	int limit;
-	int primeNumberCount = 1;
+	int limit; // Input of max prime number
 
 	cout << "This program displays prime numbers.\n";
 	cout << "Type the upper bound of the prime number you want: ";
 	cin >> limit;
-	for (int i = 0; i < limit; i++)
-	{
-		cout ;
-		if (isPrime(i))
-		{
-			cout << "Prime number " << primeNumberCount << ": " << i << "\n";
-			primeNumberCount++;
-		}
-	}
-
-	displayPrimeFactors(limit);
-	int sumPF = sumOfPrimeFactors(limit);
 	
-	for (int i = 0; i < 10; i++)
-	{
-		if (isPrime(i)) // need to rework
-		{
-			cout << i << " ";
-		}
-	}
-
-	if (isPrime(sumOfPrimeFactors(limit)))
-	{
-		cout << limit << " ";
-	}
+	//displayPrimeNumbers(limit);
+	//displayPrimeFactors(limit);
+	//displaySumOfPrimeFactors(limit);
+	displayPrimeFactorsSumPrime(limit);
+	
 
 	cout << "Done";
-	
+	return 0;
 }
---
+++ PrimeNumbers/PrimeNumbers/PrimeNumbers.h
@@ -0,0 +1,2 @@
+// System Defines
+#define COLUMNS 13 // Number of columns of prime numbers to display
